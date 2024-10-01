439c383da557ae7b1ef29b74d8d6ceee3ca1147b
millyanne93 <nmillyanne20@gmail.com>
2024-10-01 11:11:27 +0300

    Update headers and add protected route logic.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import RoleRedirectTest from './components/RoleRedirectTest';
 import AdminHomePage from './pages/AdminHomePage';
 import RegularUserHomePage from './pages/RegularUserHomePage';
 import AboutPage from './pages/AboutPage';
@@ -14,23 +13,7 @@ import EquipmentDetail from './pages/EquipmentDetail';
 import NotFound from './pages/NotFound';
 import AddEquipmentForm from './components/AddEquipmentForm';
 import ResponseInterceptor from './components/ResponseInterceptor'; // Import the ResponseInterceptor
-
-const RoleBasedRedirect = () => {
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    const role = localStorage.getItem('role');
-    if (role) {
-      if (role === 'admin') {
-        navigate('/admin-home');
-      } else {
-        navigate('/user-home');
-      }
-    }
-  }, [navigate]);
-
-  return null; // This component does not render anything
-};
+import ProtectedRoute from './components/ProtectedRoute'; // Import the ProtectedRoute
 
 function App() {
   const username = localStorage.getItem('username');
@@ -42,13 +25,29 @@ function App() {
       <main className="container mx-auto p-4 min-h-screen">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/test-role-redirect" element={<RoleRedirectTest />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/admin-home" element={<AdminHomePage username={username} />} />
-          <Route path="/user-home" element={<RegularUserHomePage username={username} />} />
+          
+          {/* Protected routes */}
+          <Route
+            path="/admin-home"
+            element={
+              <ProtectedRoute roleRequired="admin">
+                <AdminHomePage username={username} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user-home"
+            element={
+              <ProtectedRoute roleRequired="user">
+                <RegularUserHomePage username={username} />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="/equipment-detail" element={<EquipmentDetail />} />
           <Route path="/add-equipment" element={<AddEquipmentForm />} />
           <Route path="*" element={<NotFound />} />
