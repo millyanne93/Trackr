377498b667f93fc8e4a077c56ab7481b555324c6
millyanne93 <nmillyanne20@gmail.com>
2024-10-08 22:56:02 +0300

    fix vercel.json.

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('MONGO_URI:', process.env.MONGO_URI);  
     await mongoose.connect(process.env.MONGO_URI, {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB Connected...');
   } catch (err) {
