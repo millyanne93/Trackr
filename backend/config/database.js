6dd1cb7af43a541f5ab77cf0e3093d41019b4145
millyanne93 <nmillyanne20@gmail.com>
2024-10-11 10:27:27 +0300

    modify database.

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
   try {
     console.log('MONGO_URI:', process.env.MONGO_URI);  
     await mongoose.connect(process.env.MONGO_URI, {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tlsInsecure: false, 
     });
     console.log('MongoDB Connected...');
   } catch (err) {
